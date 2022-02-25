6241cc7c75103c9ebfb35ae7a41eea1f6e1d1b9c
mauf8 <mar032n@stud.kea.dk>
2022-02-25 09:31:23 +0100

    added ask about age

--- src/Demo/Main.java
+++ src/Demo/Main.java
@@ -1,8 +1,38 @@
 package Demo;
 
+import java.util.Scanner;
+
 public class Main {
+            Scanner in = new Scanner(System.in);
+
+            public void greet(){
+                System.out.println("type your name: ");
+                in.nextLine();
+                System.out.println("It works!");
+
+            }
+
+            void Go() {
+                greet();
+                askage();
+
+            }
+
+            void askage() {
+                System.out.println("Enter your age");
+                int yourage = in.nextInt();
+                System.out.println("your age is: " + yourage);
+
+            }
+
+
+
+
+
 
     public static void main(String[] args) {
-        System.out.println("It works!");
+                new Main().Go();
+
     }
+
 }
